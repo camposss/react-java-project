4384970286cfcb9a97b39eac1ebee7d332990e66
camposss <ccamposs23@gmail.com>
2018-04-07 16:16:38 -0700

    got the data while both servers are running

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
+import axios from "axios";
 
-const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
-    </div>
-);
+class App extends Component{
+
+    componentDidMount(){
+        axios({
+            method:'get',
+            url:'http://localhost:8080/reservations',
+            responseType:'stream'
+          })
+            .then(function(response) {
+                console.log(response)
+          });
+    }
+
+    render(){
+        return(
+            <div>
+                <div className="app">
+                    <img src={logo} className="logo rotate"/>
+                    <h1>Welcome to React</h1>
+                </div>
+            </div>
+        );
+    }
+
+}
 
 export default App;
